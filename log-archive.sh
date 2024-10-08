2271ff3ee765071801c4d024a931338e913f550d
Abulghani_K <72671298+Greedious@users.noreply.github.com>
2024-10-08 12:56:03 +0300

    Update log-archive.sh
    Seperating the conditions for checking if the source directory is provided and if the source directory exists

--- log-archive.sh
+++ log-archive.sh
@@ -3,16 +3,22 @@
 SOURCE_DIR=$1
 TARGET_DIR="/var/logs/$(date +"%m_%Y")"
 
-# Checking if the source directory is provided and exists
-if [ -z "$SOURCE_DIR" ] || [ ! -d "$SOURCE_DIR" ]; then
+# Checking if the source directory is provided
+if [ -z "$SOURCE_DIR" ]; then
     echo "USAGE: log-archive <log-directory>"
     exit 1
+fi
+
+# Checking if the source directory exists
+if [ ! -d "$SOURCE_DIR" ]; then
+    echo "Error: The source directory does not exist"
+    exit 2
 fi
 
 # Checking if the source directory has read permissions
 if [ ! -r "$SOURCE_DIR" ]; then 
     echo "Error: Missing read permission for the source directory: $SOURCE_DIR"
-    exit 2
+    exit 3
 fi
 
 # Create target directory if it doesn't exist
@@ -20,14 +26,14 @@ if [ ! -d "$TARGET_DIR" ]; then
     mkdir -p "$TARGET_DIR"
     if [ $? -ne 0 ]; then
         echo "Error: Failed to create target directory: $TARGET_DIR"
-        exit 3
+        exit 4
     fi
 fi
 
 # Checking if the target directory has write permissions
 if [ ! -w "$TARGET_DIR" ]; then
     echo "Error: Missing write permission for the target directory: $TARGET_DIR"
-    exit 4
+    exit 5
 fi
 
 TIMESTAMP=$(date +"%s")
@@ -41,7 +47,7 @@ echo "Creating archive..."
 tar czf "$ARCHIVE_PATH" -C "$SOURCE_DIR" . 2>/dev/null
 if [ $? -ne 0 ]; then
     echo "Error: Failed to create the archive!"
-    exit 5
+    exit 6
 fi
 
 echo "Archive created successfully: $ARCHIVE_PATH"
